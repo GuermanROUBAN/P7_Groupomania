ae16204b60cb83bd4c79cafe1183d8ac9f26d442
Guerman <guerman.rouban@gmail.com>
2021-07-08 02:30:13 +0200

    add secu

--- backend/database_connection.js
+++ backend/database_connection.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-	host: 'localhost',
-	port: 3306,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
 	dialect: 'mysql',
 	operatorsAliases: false
 });
