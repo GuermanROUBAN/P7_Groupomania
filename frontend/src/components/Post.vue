4ef7be1add0b17fe101a430a32a9fae280d515fe
Guerman <guerman.rouban@gmail.com>
2021-07-22 23:18:45 +0200

    Creation components Post

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Cours</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Devenez diplômé</h5>
+            <p class="card-text">
+              De zéro à héros, obtenez un diplôme en informatique.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <p>FOR POSTS PAGE</p>
-  </div>
+  <Post msg =''/>
 </template>
 
+
 <script>
+import Post from '../components/Post.vue' 
 export default {
   name: "Home",
+  components:{
+    Post
+  }
 };
 </script>
